a0f005b8205ff1bc509742aeae4c48aae4414cf7
reza0107 <68915561+reza0107@users.noreply.github.com>
2022-03-02 10:27:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<h1>Hallo semua, Apa kabar? </h1>
+
+Klik dibawah ini agar kita bisa kenalan lebih jauh ^_^ <br>
+<a href="test.php">My Biodata</a>
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+  $nim = "20090010";
+  $nama = "Reza Maulana Rizky";
+  $kelas = "4B";
+    echo "My Biodata <br>
+    NIM   : $nim <br>
+    Nama  : $nama <br>
+    Kelas : $kelas <br>";
+
+ print("Semoga setelah mengenal, kita bisa menyayangi");
+  
+?>
